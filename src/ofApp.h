b3a23516555539bafbd7388308dfaecde1d2c0e3
Jasper <smitj3961@gmail.com>
2017-06-16 07:58:18 +0200

    Added FF-source

--- src/ofApp.h
+++ src/ofApp.h
@@ -1,24 +1,40 @@
+// * Name: ofApp.h
+// * Project: Footfall
+// * Author: David Haylock
+// * Creation Date: 13/02/2017
+// * Copyright: (c) 2017 by Watershed Arts Trust Ltd.
+
 #pragma once
 
 #include "ofMain.h"
+#include "ConfigurationManager.h"
+#include "CameraManager.h"
+#include "TrackingManager.h"
+#include "HTTPManager.h"
+#include "CsvManager.h"
 
-class ofApp : public ofBaseApp{
+class ofApp : public ofBaseApp
+{
+	public:
+		void setup();
+		void update();
+		void draw();
+		void exit();
 
-public:
-    void setup();
-    void update();
-    void draw();
-
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y );
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-
+		void keyPressed(int key);
+		void keyReleased(int key);
+		
+		ConfigurationManager configManager;
+		CameraManager cameraManager;
+		TrackingManager trackingManager;
+		HTTPManager httpManager;
+		CsvManager csvManager;
+	
+		void blobIn(int &val);
+		void blobOut(int &val);
+	
+		bool _logToCsv;
+		bool _logToServer;
+		int peopleIn;
+		int peopleOut;
 };
